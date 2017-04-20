6dddd56cd2227e2831b7a853289112ca838ea6a4
pynkvalerian <pynkvalerian@gmail.com>
2017-04-20 22:48:24 +0800

    initialize starting state

--- app.rb
+++ app.rb
@@ -13,31 +13,5 @@ post '/' do
 
   # Start coding here, friends!
 
-  if @number >= 3 && @number < 6 && age == "months"
-    @result = "60 mg"
-
-  elsif @number > 6 && @number <= 24 && age == "months"
-    @result = "120 mg"
-
-  elsif @number >= 2 && @number < 4 && age == "years"
-    @result = "180 mg"
-
-  elsif @number >= 4 && @number < 6 && age == "years"
-    @result = "240 mg"
-
-  elsif @number >= 6 && @number < 8 && age == "years"
-    @result = "240 or 250 mg"
-
-  elsif @number >= 8 && @number < 10 && age == "years"
-    @result = "360 or 375 mg"
-
-  elsif @number >= 10 && @number < 12 && age == "years"
-    @result = "480 or 500 mg"
-
-  elsif @number >= 12 && @number < 16 && age == "years"
-    @result = "480 or 750 mg"
-    
-  end
-
   erb :home
 end
